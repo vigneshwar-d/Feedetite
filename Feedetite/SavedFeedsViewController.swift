53345f4c8c46f13a1703aa8e63fe065fef86f9c9
Vigneshwar Devendran <sendtovikki@outlook.com>
2018-03-18 14:06:34 +0530

    Add SettingsViewController and SavedFeedsViewController

---
+++ Feedetite/SavedFeedsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SavedFeedsViewController.swift
+//  Feedetite
+//
+//  Created by Vigneshwar Devendran on 18/03/18.
+//  Copyright © 2018 Vigneshwar Devendran. All rights reserved.
+//
+
+import Foundation
