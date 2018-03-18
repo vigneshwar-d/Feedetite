d020183c15ff633b02ceadb5b95d09de473116bc
Vigneshwar Devendran <sendtovikki@outlook.com>
2018-03-18 09:08:06 +0530

    Initial Commit

---
+++ Feedetite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feedetite
+//
+//  Created by Vigneshwar Devendran on 18/03/18.
+//  Copyright © 2018 Vigneshwar Devendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
