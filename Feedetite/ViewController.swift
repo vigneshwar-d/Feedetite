b164a934a5cd7222879a296726530304cde6d655
Vigneshwar Devendran <sendtovikki@outlook.com>
2018-03-22 12:20:27 +0530

    Able to get all feeds of a hardcoded link and display it on the view

--- Feedetite/ViewController.swift
+++ Feedetite/ViewController.swift
@@ -14,14 +14,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var contentsView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(contentsArray.count)
         return contentsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
         cell.cellLabel.text = contentsArray[indexPath.row]
-        print("The cell has \(cell.cellLabel.text!)")
         return cell
     }
     
@@ -38,7 +36,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         contentsView.dataSource = self
         contentsView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "customCell")
         parse()
-        print("Contents Array has \(contentsArray)")
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,26 +49,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let feedURL = URL(string: "https://www.popsci.com/rss-science.xml?loc=contentwell&lnk=science&dom=section-1")
         let parser = FeedParser(URL: feedURL!)
         let result = parser?.parse()
-        print(result?.isSuccess)
+        print((result?.isSuccess)!)
         let feeds = result?.rssFeed
-        let feedAppend = (feeds?.items?.first?.title)
-        print(feedAppend!)
-        self.contentsArray.append(feedAppend!)
-        print(feedAppend!)
-//        parser?.parse(queue: DispatchQueue.global(qos: .userInitiated)) { (result) in
-//            // Do your thing, then back to the Main thread
-//            DispatchQueue.main.async {
-//                // ..and update the UI
-//                print(result.isSuccess)
-//                let feeds = result.rssFeed
-//                let feedAppend = (feeds?.items?.first?.title)!
-//                print(feedAppend)
-////                let contentObj = FeedsContents()
-////                contentObj.feedTitle = feedAppend
-//                self.contentsArray.append(feedAppend)
-//                print(self.contentsArray)
-//            }
-//        }
+        for i in 0..<((feeds?.items?.count)!){
+            var it = i
+            let feedAppend = (feeds?.items![it].title)
+            it = it + 1
+            print("Value in feedAppend \(feedAppend!)")
+            self.contentsArray.append(feedAppend!)
+            //print((feeds?.items![1].title)!)
+            print((feeds?.items?.count)!)
+        }
+//        let feedAppend = (feeds?.items?.first?.title)
+//        print("Value in feedAppend \(feedAppend!)")
+//        self.contentsArray.append(feedAppend!)
+//        print((feeds?.items![1].title)!)
+//        print((feeds?.items?.count)!)
     }
 
     @IBAction func settingsButtonPressed(_ sender: Any) {
