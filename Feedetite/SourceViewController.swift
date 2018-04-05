469a23784089ddedeaf01a0dc16c8b96a57ccb47
Vigneshwar Devendran <sendtovikki@outlook.com>
2018-04-05 10:56:15 +0530

    Able to make segues and pass data

---
+++ Feedetite/SourceViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SourceViewController.swift
+//  Feedetite
+//
+//  Created by Vigneshwar Devendran on 05/04/18.
+//  Copyright © 2018 Vigneshwar Devendran. All rights reserved.
+//
+
+import UIKit
+
+class SourceViewController: UITableViewController{
+    
+    
+    //MARK: - Initializers
+    let sources = ["CNN","CNN Technology"]
+    let sourcesURL = ["",""]
+    
+    
+    override func viewDidLoad() {
+        tableView.rowHeight = 70
+    }
+    
+    
+    //MARK: - TableView Functions
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sourceCell", for: indexPath)
+        cell.textLabel?.text = sources[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToFeed", sender: self)
+    }
+    
+    
+    //MARK: - Prepare For Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! FeedListController
+        destinationVC.feedSourceUrl = sourcesURL[(tableView.indexPathForSelectedRow?.row)!]
+        destinationVC.feedSourceName = sources[(tableView.indexPathForSelectedRow?.row)!]
+    }
+}
